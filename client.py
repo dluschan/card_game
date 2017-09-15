5e3934fddc29a43738fa3a9df14451b94fe0cab9
Dmitry Luschan <dluschan@gmail.com>
2017-09-16 01:31:40 +0300

    добавлена интерактивность в клиент. соединение теперь устанавливается только по запросу

--- client.py
+++ client.py
@@ -7,20 +7,38 @@ class Client:
         self.coins = 1000
         self.id = None
         self.server = None
-        self.s = sender.Sender()
+        self.s = None
 
     def send(self, msg):
+        if self.s is None:
+            self.s = sender.Sender()
         self.s.send(msg)
 
     def recv(self):
         return self.s.recv()
 
 player = Client()
-player.send('Hi!')
-player.id = player.recv()
-print(player.id)
-
-for i in range(2):
-    player.cards.append(player.recv())
-for c in player.cards:
-    print(str(c))
+ans = ''
+while ans != 'q':
+    print('r - registration')
+    print('w - wait')
+    print('s - show')
+    print('q - quit')
+    ans = input('Введите команду(r/w/s/q): ')
+    if ans == 'r':
+        player.send('Hi!')
+        player.id = player.recv()
+        print("Регистрация на сервере успешно выполнена", player.id)
+    elif ans == 'w':
+        print('Запрос карт')
+        player.send('Request')
+        while (len(player.cards)) < 2:
+            player.cards.append(player.recv())
+    elif ans == 'q':
+        print('By!')
+        exit(0)
+    elif ans == 's':
+        for c in player.cards:
+            print(str(c))
+    else:
+        print('Команда не распознана')
