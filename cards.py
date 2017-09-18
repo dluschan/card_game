258c48c33d7ea9bb718b2eca6a1420110ce43c08
Dmitry Luschan <dluschan@gmail.com>
2017-09-18 23:21:04 +0300

    замена символов мастей карт

--- cards.py
+++ cards.py
@@ -3,19 +3,19 @@ class Suit:
 
 class Diamonds(Suit):
     def __str__(self):
-        return '♦︎'
+        return '♢'
 
 class Clubs(Suit):
     def __str__(self):
-        return '♣︎'
+        return '♧'
 
 class Hearts(Suit):
     def __str__(self):
-        return '♥︎'
+        return '♡'
 
 class Spades(Suit):
     def __str__(self):
-        return '♠︎'
+        return '♤'
 
 class Value:
     value_map = {0: 'A', 1: '2', 2: '3', 3: '4', 4: '5', 5: '6', 6: '7', 7: '8', 8: '9', 9: '10', 10: 'J', 11: 'Q', 12: 'K'}
